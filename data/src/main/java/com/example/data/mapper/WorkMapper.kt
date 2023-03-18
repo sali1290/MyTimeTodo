2c5a268fdadb9a881a7f5fce58ac74b3f89aa734
Ali Nabavi <arz1379n@gmail.com>
2023-03-18 22:27:49 +0330

    implement mapper class

---
+++ data/src/main/java/com/example/data/mapper/WorkMapper.kt
@@ -0,0 +1,29 @@
+package com.example.data.mapper
+
+import com.example.data.model.WorkModel
+import com.example.domain.model.Work
+
+class WorkMapper {
+
+    fun toWork(workModel: WorkModel): Work {
+        return Work(
+            id = workModel.id,
+            title = workModel.title,
+            body = workModel.body,
+            time = workModel.time,
+            color = workModel.color,
+        )
+    }
+
+    fun toWorkModel(work: Work): WorkModel {
+        return WorkModel(
+            id = work.id,
+            title = work.title,
+            body = work.body,
+            time = work.time,
+            color = work.color,
+        )
+    }
+
+
+}

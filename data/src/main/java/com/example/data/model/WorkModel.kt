15f5db12174b1f4129fe73c177910ea64f68f057
Ali Nabavi <arz1379n@gmail.com>
2023-03-18 14:47:29 +0330

    create project work models

---
+++ data/src/main/java/com/example/data/model/WorkModel.kt
@@ -0,0 +1,15 @@
+package com.example.data.model
+
+data class WorkModel(
+
+    val id: Int,
+
+    val title: String,
+
+    val time: String,
+
+    val body: String,
+
+    val color: String
+
+)
---
+++ domain/src/main/java/com/example/domain/model/Work.kt
@@ -0,0 +1,15 @@
+package com.example.domain.model
+
+data class Work(
+
+    val id: Int,
+
+    val title: String,
+
+    val time: String,
+
+    val body: String,
+
+    val color: String
+
+)

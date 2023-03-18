88a38eb7283df52fa5685be68e75bee0e867b4f1
Ali Nabavi <arz1379n@gmail.com>
2023-03-18 14:39:09 +0330

    implement packages of project and App class

--- app/src/main/java/com/example/mytimetodo/activity/MainActivity.kt
+++ app/src/main/java/com/example/mytimetodo/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mytimetodo
+package com.example.mytimetodo.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mytimetodo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

5427db185a8887d9308deefd4b6a809e74a448c8
Ali Nabavi <arz1379n@gmail.com>
2023-03-20 19:25:43 +0330

    implement navigation component

--- app/src/main/java/com/example/mytimetodo/activity/MainActivity.kt
+++ app/src/main/java/com/example/mytimetodo/activity/MainActivity.kt
@@ -1,46 +1,19 @@
 package com.example.mytimetodo.activity
 
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mytimetodo.databinding.ActivityMainBinding
-import com.example.mytimetodo.viewmodel.HomeViewModel
-import com.example.mytimetodo.utility.Result
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: HomeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        viewModel.getAllWorks()
-        observe()
-    }
-
-    private fun observe() {
-        viewModel.works.observe(this) {
-            when (it) {
-
-                is Result.Success -> {
-                    Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
-                }
-
-                is Result.Loading -> {
-                    Toast.makeText(this, "Loading", Toast.LENGTH_LONG).show()
-                }
-
-                is Result.Error -> {
-                    Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
-                }
-
-            }
-        }
 
     }
 
---
+++ app/src/main/java/com/example/mytimetodo/fragment/HomeFragment.kt
@@ -0,0 +1,57 @@
+package com.example.mytimetodo.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.mytimetodo.databinding.FragmentHomeBinding
+import com.example.mytimetodo.utility.Result
+import com.example.mytimetodo.viewmodel.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getAllWorks()
+        observe()
+
+
+    }
+
+    private fun observe() {
+        viewModel.works.observe(viewLifecycleOwner) {
+            when (it) {
+
+                is Result.Success -> {
+                    Toast.makeText(requireContext(), "Success", Toast.LENGTH_LONG).show()
+                }
+
+                is Result.Loading -> {
+                    Toast.makeText(requireContext(), "Loading", Toast.LENGTH_LONG).show()
+                }
+
+                is Result.Error -> {
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                }
+
+            }
+        }
+    }
+}

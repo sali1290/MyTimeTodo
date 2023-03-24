092c6bfd541be0dcc958cd321dd7e640086bba91
Ali Nabavi <arz1379n@gmail.com>
2023-03-24 20:49:07 +0330

    implement bottom navigation bar with fab

--- app/src/main/java/com/example/mytimetodo/activity/MainActivity.kt
+++ app/src/main/java/com/example/mytimetodo/activity/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.bottomNavigationView.background = null
     }
 
 }

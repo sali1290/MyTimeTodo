a3a060e2ca16ec820022a3f2eaae757bc16b677c
Ali Nabavi <arz1379n@gmail.com>
2023-04-04 00:21:21 +0330

    implement EditWorkFragment.kt for update existing work

--- app/src/main/java/com/example/mytimetodo/fragment/EditWorkFragment.kt
+++ app/src/main/java/com/example/mytimetodo/fragment/EditWorkFragment.kt
@@ -1,22 +1,32 @@
 package com.example.mytimetodo.fragment
 
+import android.app.TimePickerDialog
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.domain.model.Work
 import com.example.mytimetodo.R
 import com.example.mytimetodo.adapter.WorkColorList
 import com.example.mytimetodo.adapter.WorkColorsAdapter
 import com.example.mytimetodo.adapter.changeBackgroundColor
 import com.example.mytimetodo.databinding.FragmentEditWorkBinding
+import com.example.mytimetodo.viewmodel.HomeViewModel
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
 class EditWorkFragment : Fragment() {
@@ -25,7 +35,11 @@ class EditWorkFragment : Fragment() {
     private val binding: FragmentEditWorkBinding
         get() = _binding!!
 
+    private val viewModel: HomeViewModel by viewModels()
     private lateinit var adapter: WorkColorsAdapter
+
+    private val calendar = Calendar.getInstance()
+    private lateinit var work: Work
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,6 +56,8 @@ class EditWorkFragment : Fragment() {
         onBackPressed()
         setUpColorRecycler()
         getWorkForEdit()
+        setUpOnClicks()
+        observe()
     }
 
     override fun onDestroyView() {
@@ -53,14 +69,87 @@ class EditWorkFragment : Fragment() {
         setFragmentResultListener("requestKey") { _, bundle ->
             //any type that can be put in a Bundle is supported
             //maybe in future change this line to parcelable
-            val work = bundle.getSerializable("workKey") as Work
+            work = bundle.getSerializable("workKey") as Work
             //do something with the result
+            binding.chbDaily.isChecked = work.time != null
+
             binding.etWorkTitle.setText(work.title)
             binding.etWorkBody.setText(work.body)
             binding.etWorkBody.setBackgroundColor(
                 work.color.toInt()
             )
         }
+    }
+
+    private fun setUpOnClicks() {
+        binding.btnCancel.setOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+
+        binding.btnSave.setOnClickListener {
+            val title = binding.etWorkTitle.text.toString()
+            val body = binding.etWorkBody.text.toString()
+            val isRoutine = binding.chbDaily.isChecked
+            var date: Date?
+
+
+            if (title.isEmpty()) {
+                binding.etWorkTitle.error = "Work Title shouldn't be empty"
+            } else {
+                if (body.isEmpty()) {
+                    binding.etWorkBody.error = "Work body shouldn't be empty"
+                } else {
+                    //check if work is daily or other
+                    if (isRoutine && work.time != null) {
+                        //daily works
+                        //show TimePickerDialog
+                        TimePickerDialog(
+                            requireActivity(),
+                            { _, hourOfDay, minute ->
+                                calendar.set(
+                                    calendar.get(Calendar.YEAR),
+                                    calendar.get(Calendar.MONTH),
+                                    calendar.get(Calendar.DAY_OF_MONTH),
+                                    hourOfDay,
+                                    minute
+                                )
+                                date = calendar.time
+
+
+                                viewModel.updateWork(
+                                    Work(
+                                        id = work.id,
+                                        title = title,
+                                        body = body,
+                                        time = date,
+                                        color = (binding.etWorkBody.background as (ColorDrawable)).color.toString()
+                                    )
+                                )
+                                //navigate to daily routine fragment and pop this fragment
+                                successfulWorkEdit()
+                            },
+                            calendar.get(Calendar.HOUR_OF_DAY),
+                            calendar.get(Calendar.MINUTE),
+                            true
+                        ).show()
+                    } else {
+                        //other works
+                        viewModel.updateWork(
+                            Work(
+                                id = work.id,
+                                title = title,
+                                body = body,
+                                time = null,
+                                color = (binding.etWorkBody.background as (ColorDrawable)).color.toString()
+                            )
+                        )
+                        //navigate to daily routine fragment and pop this fragment
+                        successfulWorkEdit()
+                    }
+                }
+            }
+        }
+
     }
 
     private fun setUpColorRecycler() {
@@ -97,6 +186,45 @@ class EditWorkFragment : Fragment() {
                 }
             }
             )
+    }
+
+    private fun observe() {
+
+        viewModel.updateResult.observe(viewLifecycleOwner) {
+            if (it) {
+                successfulWorkEdit()
+            } else {
+                Toast.makeText(
+                    requireActivity(),
+                    "Something went wrong! please try again",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+
+    }
+
+    private fun successfulWorkEdit() {
+        val snackBar = Snackbar.make(
+            requireContext(),
+            binding.btnSave,
+            "Work saved",
+            Snackbar.LENGTH_SHORT
+        )
+        (snackBar.view.layoutParams as (FrameLayout.LayoutParams)).gravity =
+            Gravity.TOP
+        snackBar.setAction("Ok") {
+            snackBar.dismiss()
+        }
+        snackBar.show()
+        requireActivity().apply {
+            findViewById<FloatingActionButton>(R.id.fab).visibility =
+                View.VISIBLE
+            findViewById<BottomAppBar>(R.id.bottom_app_bar).visibility =
+                View.VISIBLE
+        }
+        findNavController().navigate(R.id.dailyRoutineFragment)
     }
 
 }
